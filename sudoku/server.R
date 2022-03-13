3cd237a0687f6a33706c1c1956b9e17880828dc6
lucea97217 <lucea.lenny@hotmail.com>
2022-03-13 13:00:08 +0100

    .

--- sudoku/server.R
+++ sudoku/server.R
@@ -8,6 +8,13 @@ shinyServer(function(input, output, session) {
 
 
     # Definition fonction sudoku
+
+    # Nous avons donc ici deux façons conceptions de la "difficulté" soit nous nous fions
+    # Au "backtracking" dans ce cas nous nous fions au nombre de retours
+    # Soit nous partons du principe que plus il y a de "blancs dans un soduko plus la
+    # Difficulté sera conséquente. Il faudra alors modifier freq.mean et/ou freq.max
+    # Comme demandé dans l'énoncé du tp nous nous fixons à attempt pour étudier la difficulté
+    # Mais nous pouvions très bien procéder autrement.
     observeEvent(input$load,{
         sudo <- danmsudo()
         sudo = "Fail"
